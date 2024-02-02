d38f765082df661ac6fba736355a4717a41f24ad
Deimos <alex279368@gmail.com>
2024-02-02 14:02:45 +0200

    Update setup scripts

--- src/scripts/setup.ts
+++ src/scripts/setup.ts
@@ -12,117 +12,97 @@ const rl = readline.createInterface({
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-let currentDir = __dirname;
-while (!fs.existsSync(path.join(currentDir, 'package.json'))) {
-    const parentDir = path.dirname(currentDir);
-    if (parentDir === currentDir) {
-        throw new Error('Не вдалося знайти корінь проекту');
+function findProjectRoot(currentDir: string): string {
+    while (!fs.existsSync(path.join(currentDir, 'package.json'))) {
+        const parentDir = path.dirname(currentDir);
+        if (parentDir === currentDir) {
+            throw new Error('Не вдалося знайти корінь проекту');
+        }
+        currentDir = parentDir;
     }
-    currentDir = parentDir;
+    return currentDir;
 }
 
+function readEnvironmentFile(envPath: string): string[] {
+    return fs.existsSync(envPath) ? fs.readFileSync(envPath, 'utf-8').split('\n') : [];
+}
+
+function updateOrInsertEnvVariable(lines: string[], variable: string, value: string): void {
+    const index = lines.findIndex(line => line.startsWith(`${variable}=`));
+    if (index !== -1) {
+        lines[index] = `${variable}=${value}`;
+    } else {
+        lines.push(`${variable}=${value}`);
+    }
+}
+
+function writeEnvironmentFile(envPath: string, lines: string[]): void {
+    fs.writeFileSync(envPath, lines.join('\n'));
+}
+
+function installNpmDependencies(): void {
+    console.log('Встановлюємо npm залежності...');
+    exec('npm install', (error, stdout, stderr) => {
+        if (error) {
+            console.log(`Помилка при встановленні npm залежностей: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`Стандартна помилка: ${stderr}`);
+            return;
+        }
+        console.log(`Результат: ${stdout}`);
+    });
+}
+
+console.log(`\x1b[35mЦей скрипт допоможе вам налаштувати телаграм бот.\x1b[0m`);
+
+const currentDir = findProjectRoot(__dirname);
 const root = currentDir;
 const envPath = path.join(root, '.env');
 const readmeMD = path.join(root, 'README.md');
 
-console.log(`\x1b[35mЦей скрипт допоможе вам налаштувати телаграм бот.\x1b[0m`);
+rl.question('Чи хочете ви оновити конфігурацію оточення? (yes/no): ', (answer: string) => {
+    if (answer.toLowerCase() === 'yes') {
+        console.log(`Як дізнатися ADMIN_ID та GROUP_ID читайте в \x1b[34m${readmeMD}\x1b[0m`);
+        rl.question('Введіть ADMIN_ID: ', (adminId: string) => {
+            rl.question('Введіть GROUP_ID: ', (groupId: string) => {
+                rl.question('Введіть MONGO_DB_USER: ', (mongoDbUser: string) => {
+                    rl.question('Введіть MONGO_DB_PASSWORD: ', (mongoDbPassword: string) => {
+                        rl.question('Введіть MONGO_DB_HOST: ', (mongoDbHost: string) => {
+                            rl.question('Введіть PRODUCTION_BOT_TOKEN: ', (productionBotToken: string) => {
+                                const envLines = readEnvironmentFile(envPath);
 
-rl.question(
-    'Чи хочете ви ввести ADMIN_ID та GROUP_ID? (yes/no): ',
-    (answer: string) => {
-        if (answer.toLowerCase() === 'yes') {
-            console.log(
-                `Як дізнатися ADMIN_ID та GROUP_ID читайте в \x1b[34m${readmeMD}\x1b[0m`
-            );
-            rl.question('Введіть ADMIN_ID: ', (adminId: string) => {
-                rl.question('Введіть GROUP_ID: ', (groupId: string) => {
-                    let env = fs.existsSync(envPath)
-                        ? fs.readFileSync(envPath, 'utf-8')
-                        : '';
-                    let lines = env.split('\n');
+                                updateOrInsertEnvVariable(envLines, 'ADMIN_ID', adminId);
+                                updateOrInsertEnvVariable(envLines, 'GROUP_ID', groupId);
+                                updateOrInsertEnvVariable(envLines, 'MONGO_DB_USER', mongoDbUser);
+                                updateOrInsertEnvVariable(envLines, 'MONGO_DB_PASSWORD', mongoDbPassword);
+                                updateOrInsertEnvVariable(envLines, 'MONGO_DB_HOST', mongoDbHost);
+                                updateOrInsertEnvVariable(envLines, 'PRODUCTION_BOT_TOKEN', productionBotToken);
 
-                    let adminIdExists = false;
-                    let groupIdExists = false;
+                                writeEnvironmentFile(envPath, envLines);
 
-                    lines = lines.map((line) => {
-                        if (line.startsWith('ADMIN_ID=')) {
-                            adminIdExists = true;
-                            return `ADMIN_ID=${adminId}`;
-                        } else if (line.startsWith('GROUP_ID=')) {
-                            groupIdExists = true;
-                            return `GROUP_ID=${groupId}`;
-                        } else {
-                            return line;
-                        }
-                    });
+                                console.log('Конфігурація оточення успішно оновленна!');
 
-                    if (!adminIdExists) {
-                        lines.push(`ADMIN_ID=${adminId}`);
-                    }
-
-                    if (!groupIdExists) {
-                        lines.push(`GROUP_ID=${groupId}`);
-                    }
-
-                    env = lines.join('\n');
-                    fs.writeFileSync(envPath, env);
-                    console.log(
-                        'ADMIN_ID та GROUP_ID були успішно записані в .env файл.'
-                    );
-
-                    rl.question(
-                        'Чи хочете ви встановити npm залежності? (yes/no): ',
-                        (answer: string) => {
-                            if (answer.toLowerCase() === 'yes') {
-                                console.log('Встановлюємо npm залежності...');
-                                exec('npm install', (error, stdout, stderr) => {
-                                    if (error) {
-                                        console.log(
-                                            `Помилка при встановленні npm залежностей: ${error.message}`
-                                        );
-                                        return;
+                                rl.question('Чи хочете ви оновити npm залежності? (yes/no): ', (answer: string) => {
+                                    if (answer.toLowerCase() === 'yes') {
+                                        installNpmDependencies();
                                     }
-                                    if (stderr) {
-                                        console.log(
-                                            `Стандартна помилка: ${stderr}`
-                                        );
-                                        return;
-                                    }
-                                    console.log(`Результат: ${stdout}`);
+                                    rl.close();
                                 });
-                            }
-
-                            rl.close();
-                        }
-                    );
+                            });
+                        });
+                    });
                 });
             });
-        } else {
-            console.log('Пропускаємо крок введення ADMIN_ID та GROUP_ID.');
+        });
+    } else {
 
-            rl.question(
-                'Чи хочете ви встановити npm залежності? (yes/no): ',
-                (answer: string) => {
-                    if (answer.toLowerCase() === 'yes') {
-                        console.log('Встановлюємо npm залежності...');
-                        exec('npm install', (error, stdout, stderr) => {
-                            if (error) {
-                                console.log(
-                                    `Помилка при встановленні npm залежностей: ${error.message}`
-                                );
-                                return;
-                            }
-                            if (stderr) {
-                                console.log(`Стандартна помилка: ${stderr}`);
-                                return;
-                            }
-                            console.log(`Результат: ${stdout}`);
-                        });
-                    }
-
-                    rl.close();
-                }
-            );
-        }
+        rl.question('Чи хочете ви оновити npm залежності? (yes/no): ', (answer: string) => {
+            if (answer.toLowerCase() === 'yes') {
+                installNpmDependencies();
+            }
+            rl.close();
+        });
     }
-);
+});
