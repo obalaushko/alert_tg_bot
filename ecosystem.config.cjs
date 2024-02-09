6380a5655656d0ed0bf7ce201028a1a36a45112c
Deimos <alex279368@gmail.com>
2024-02-09 15:34:16 +0200

    Minor update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,25 +1,21 @@
 module.exports = {
-  apps: [
-    {
-      name: 'telegram_alert_bot',
-      script: 'npx',
-      args: 'npm run start',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production',
-        PATH: '/home/deimos/.nvm/versions/node/v20.9.0/bin/:$PATH',
-      },
-      exec_mode: 'fork',
-      exec_interpreter: 'bash',
-      post_update: [
-        'git pull',
-        'npm run build',
-      ],
-      output: './logs/out.log',
-      error: './logs/error.log'
-    },
-  ],
+    apps: [
+        {
+            name: 'telegram_alert_bot',
+            script: 'npx',
+            args: 'npm run start',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'production',
+            },
+            exec_mode: 'fork',
+            exec_interpreter: 'bash',
+            post_update: ['git pull', 'npm run build'],
+            output: './logs/out.log',
+            error: './logs/error.log',
+        },
+    ],
 };
