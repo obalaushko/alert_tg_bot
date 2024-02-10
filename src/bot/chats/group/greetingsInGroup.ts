c76dd1f2e790fdfbf2ef20c1b530da716748bbdb
Deimos <alex279368@gmail.com>
2024-02-10 14:08:38 +0200

    Minor update

--- src/bot/chats/group/greetingsInGroup.ts
+++ src/bot/chats/group/greetingsInGroup.ts
@@ -1,19 +1,25 @@
 import moment from 'moment-timezone';
+import * as dotenv from 'dotenv';
 
 import { MSG } from '../../../constants/messages.js';
 import { bot } from '../../bot.js';
+
+dotenv.config();
 
 export const greetingsInGroup = async () => {
     try {
         const now = moment().tz('Europe/Kiev');
         const isWeekday = now.isoWeekday() < 6;
+        const GROUP_ID = process.env.GROUP_ID || '';
+
+        if (!GROUP_ID) {
+            console.error('Error: GROUP_ID is not defined');
+            return;
+        }
 
         if (isWeekday) {
             // TMP group id
-            await bot.api.sendMessage(
-                -1001992031620,
-                MSG.groups.greetings.static
-            );
+            await bot.api.sendMessage(GROUP_ID, MSG.groups.greetings.static);
 
             console.log('Greetings sent!');
         }
